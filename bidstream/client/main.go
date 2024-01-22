0e017e6b6db7bb1e498cdf7c41b7414ac85dd11b
Ravi Shankar <rashank2@cisco.com>
2024-01-22 09:28:08 +0530

    New commit.

--- bidstream/client/main.go
+++ bidstream/client/main.go
@@ -7,21 +7,42 @@ import (
 	"log"
 	"os"
 	"strconv"
-	"sync"
 	"time"
 
 	pb "github.com/RaviGitCisco/grpc-go-course/bidstream/proto"
 	"google.golang.org/grpc"
 )
 
-type client struct {
-	sendCh chan *pb.Message
-	recvCh chan *pb.Message
+//type client struct{}
+
+func startStreaming(client pb.BidirectionalService_BidirectionalStreamClient, interval int) {
+	for {
+		req := &pb.Request{Content: "Client request"}
+		if err := client.Send(req); err != nil {
+			log.Printf("Error sending request: %v", err)
+			return
+		}
+
+		for {
+			res, err := client.Recv()
+			if err == io.EOF {
+				log.Println("Server closed the connection.")
+				return
+			}
+			if err != nil {
+				log.Printf("Error receiving response: %v", err)
+				return
+			}
+			fmt.Printf("Received response: %s\n", res.Content)
+			time.Sleep(5 * time.Second) // Adjust interval as needed
+		}
+
+	}
 }
 
 func main() {
-	if len(os.Args) < 3 {
-		fmt.Println("Usage: client <interval_seconds> <message_count>")
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: client <interval_seconds>")
 		os.Exit(1)
 	}
 
@@ -30,70 +51,17 @@ func main() {
 		log.Fatalf("Invalid interval: %v", err)
 	}
 
-	messageCount, err := strconv.Atoi(os.Args[2])
-	if err != nil {
-		log.Fatalf("Invalid message count: %v", err)
-	}
-
 	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("Failed to connect: %v", err)
 	}
 	defer conn.Close()
 
-	clientInstance := &client{
-		sendCh: make(chan *pb.Message),
-		recvCh: make(chan *pb.Message),
-	}
-
 	client := pb.NewBidirectionalServiceClient(conn)
-
 	stream, err := client.BidirectionalStream(context.Background())
 	if err != nil {
 		log.Fatalf("Error creating stream: %v", err)
 	}
 
-	var sendWg sync.WaitGroup
-	var recvWg sync.WaitGroup
-
-	// Handle incoming messages
-	recvWg.Add(1)
-	go func() {
-		defer recvWg.Done()
-		defer close(clientInstance.recvCh)
-		for {
-			msg, err := stream.Recv()
-			if err == io.EOF {
-				return
-			}
-			if err != nil {
-				log.Printf("Error receiving message: %v", err)
-				return
-			}
-			fmt.Printf("Received message: %s\n", msg.Content)
-			clientInstance.recvCh <- msg
-		}
-	}()
-
-	// Handle outgoing messages
-	sendWg.Add(1)
-	go func() {
-		defer sendWg.Done()
-		for i := 0; i < messageCount; i++ {
-			clientInstance.sendCh <- &pb.Message{Content: fmt.Sprintf("Client message %d", i)}
-			log.Println("Sent message sleeping 5 seconds")
-			time.Sleep(time.Duration(interval) * time.Second)
-		}
-		log.Println("closing sendch")
-		close(clientInstance.sendCh)
-	}()
-
-	// Wait for both goroutines to finish
-	sendWg.Wait()
-	// Close the stream properly after sending all messages
-	if err := stream.CloseSend(); err != nil {
-		log.Printf("Error closing the stream: %v", err)
-	}
-	close(clientInstance.sendCh)
-	recvWg.Wait()
+	startStreaming(stream, interval)
 }
--- bidstream/server/main.go
+++ bidstream/server/main.go
@@ -1,123 +1,95 @@
 package main
 
 import (
+	"bufio"
+	"context"
 	"fmt"
 	"io"
 	"log"
 	"net"
+	"os"
 	"sync"
 	"time"
 
 	pb "github.com/RaviGitCisco/grpc-go-course/bidstream/proto"
+
 	"google.golang.org/grpc"
 )
 
 type server struct {
 	pb.BidirectionalServiceServer
-	sendCh    chan *pb.Message
-	recvCh    chan *pb.Message
-	timeout   time.Duration
-	closeOnce sync.Once
-	closeCh   chan struct{}
 }
 
 func (s *server) BidirectionalStream(stream pb.BidirectionalService_BidirectionalStreamServer) error {
 	var wg sync.WaitGroup
+	ctx, cancel := context.WithCancel(context.Background())
+	timer := time.NewTimer(10 * time.Second)
 
-	// Handle incoming messages
+	// Handle incoming requests
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
 		for {
 			select {
-			case <-s.closeCh:
-				log.Println("Close channel message received.")
+			case <-ctx.Done():
+				return
+			case <-timer.C:
+				log.Println("Connection timed out. Closing.")
+				cancel()
 				return
 			default:
-				msg, err := stream.Recv()
+				req, err := stream.Recv()
 				if err == io.EOF {
-					log.Println("Stream received EOF message.")
 					return
 				}
 				if err != nil {
-					log.Printf("Error receiving message: %v", err)
-					s.closeConnection()
+					log.Printf("Error receiving request: %v", err)
 					return
 				}
-				fmt.Printf("Received message: %s\n", msg.Content)
-				s.recvCh <- msg
-			}
-		}
-	}()
+				fmt.Printf("Received request: %s\n", req.Content)
 
-	// Handle outgoing messages
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		for {
-			select {
-			case <-s.closeCh:
-				log.Println("Closing the Send channel.")
-				return
-			case msg := <-s.sendCh:
-				if msg == nil {
-					// Log an error or handle the nil message case appropriately
-					log.Printf("Error: Attempted to send nil message.")
-					return
-				}
-				if err := stream.Send(msg); err != nil {
-					log.Printf("Error sending message: %v", err)
-					s.closeConnection()
-					return
-				}
-			}
-		}
-	}()
-
-	// Monitor timeout for receiving messages
-	go func() {
-		timer := time.NewTimer(s.timeout)
-		defer timer.Stop()
-		log.Println("Timer started to receive messages from client")
-		for {
-			select {
-			case <-timer.C:
-				log.Println("Connection timed out. Closing.")
-				s.closeConnection()
-				return
-			case <-s.recvCh:
-				// Reset the timer if a message is received
+				// Reset the timer since a request was received
 				if !timer.Stop() {
-					log.Println("Stop timer block.")
 					<-timer.C
 				}
-				log.Println("Resetting the timer")
-				timer.Reset(s.timeout)
-			case <-s.closeCh:
-				log.Println("Closing the channel")
-				return
+				timer.Reset(10 * time.Second)
+
+				s.sendResponses(stream)
 			}
-			log.Println("I am looping here.")
 		}
 	}()
 
-	// Wait for both goroutines to finish
+	// Monitor timeout for incoming requests
+	go func() {
+		<-time.After(10 * time.Second)
+		log.Println("Connection timed out. Closing.")
+		cancel()
+	}()
+
+	// Wait for the incoming requests goroutine to finish
 	wg.Wait()
 	return nil
 }
 
-func (s *server) closeConnection() {
-	s.closeOnce.Do(func() {
-		log.Println("Closing the channel")
-		close(s.closeCh)
-	})
-}
+func (s *server) sendResponses(stream pb.BidirectionalService_BidirectionalStreamServer) {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Enter a response (or type 'exit' to close): ")
+		scanner.Scan()
+		response := scanner.Text()
 
-// The following methods are required to implement BidirectionalServiceServer interface
-//func (s *server) BidirectionalServiceServer(ctx context.Context, request *pb.Message) (*pb.Message, error) {
-// Implement the method logic here
-//return &pb.Message{}, nil
-//}
+		if response == "exit" {
+			fmt.Println("Closing connection as requested.")
+			return
+		}
+
+		res := &pb.Response{Content: response}
+		if err := stream.Send(res); err != nil {
+			log.Printf("Error sending response: %v", err)
+			return
+		}
+	}
+}
 
 func main() {
 	listener, err := net.Listen("tcp", ":50051")
@@ -126,43 +98,11 @@ func main() {
 	}
 
 	srv := grpc.NewServer()
+	servinst := &server{}
+	pb.RegisterBidirectionalServiceServer(srv, servinst)
 
-	sendCh := make(chan *pb.Message)
-	recvCh := make(chan *pb.Message)
-	closeCh := make(chan struct{})
-
-	serverInstance := &server{
-		sendCh:  sendCh,
-		recvCh:  recvCh,
-		timeout: 120 * time.Second,
-		closeCh: closeCh,
+	log.Println("Server is listening on :50051")
+	if err := srv.Serve(listener); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
 	}
-
-	pb.RegisterBidirectionalServiceServer(srv, serverInstance)
-
-	go func() {
-		log.Println("Server is listening on :50051")
-		if err := srv.Serve(listener); err != nil {
-			log.Fatalf("Failed to serve: %v", err)
-		}
-		log.Println("Listner routine started.")
-	}()
-
-	// Simulate sending messages to the server
-	go func() {
-		for i := 0; i < 5; i++ {
-			msg := &pb.Message{Content: fmt.Sprintf("Server response %d", i)}
-			serverInstance.sendCh <- msg
-			log.Printf("Sending message %v", msg.Content)
-		}
-		close(serverInstance.sendCh)
-		log.Println("Exiting send routine.")
-	}()
-
-	// Wait for user input to exit
-	fmt.Println("Press Enter to exit...")
-	fmt.Scanln()
-	close(serverInstance.sendCh)
-	close(serverInstance.recvCh)
-	srv.GracefulStop()
 }
